da73e715841e31f8c2457f21eda68a6bba9adcb5
alumnog <alumnog@S923_7.itson.edu.mx>
2023-09-08 11:18:48 -0700

    Version inicial del pryecto

---
+++ src/mx/itson/tortilla/negocio/Operacion.java
@@ -0,0 +1,24 @@
+
+package mx.itson.tortilla.negocio;
+/**
+*contine metodos diversos para trabajar con cadenas de texto.
+*@author Miguel T.
+*/
+
+public class Operacion {
+
+/**
+*separa una cadena de texto en donde encuentre el caracter guion medio.
+*@param oracion es la oracion que sera separada en fracciones
+*@return Array de String en un cuyo cada componente sera un fraccion de la cadena otorgada
+*/
+    
+public  String[] separar(String oracion){
+  String[] resultado = oracion.split(",");
+return resultado;
+}
+
+
+
+
+}
